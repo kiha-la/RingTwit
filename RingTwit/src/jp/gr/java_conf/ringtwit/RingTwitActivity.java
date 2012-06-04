b8b130cb5c639032689883f18c2eb08d2a634a55
khara <khara@192.168.24.59>
2012-06-04 22:58:27 +0900

    test

--- RingTwit/src/jp/gr/java_conf/ringtwit/RingTwitActivity.java
+++ RingTwit/src/jp/gr/java_conf/ringtwit/RingTwitActivity.java
@@ -1,13 +1,97 @@
 package jp.gr.java_conf.ringtwit;
 
-import android.app.Activity;
-import android.os.Bundle;
 
-public class RingTwitActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+import twitter4j.TwitterException;
+import twitter4j.auth.OAuthAuthorization;
+import twitter4j.auth.RequestToken;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationContext;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class RingTwitActivity extends Activity implements OnClickListener {
+	/** Called when the activity is first created. */
+
+	public static RequestToken _req = null;
+	public static OAuthAuthorization _oauth = null;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// ボタン設定
+		Button startServiceButton = (Button) findViewById(R.id.button1);
+		Button authorizeButton = (Button) findViewById(R.id.button2);
+		startServiceButton.setOnClickListener(this);
+		authorizeButton.setOnClickListener(this);
+
+	}
+
+	protected void onResume() {
+		super.onRestart();
+
+		if ( _req !=null) {
+			Log.d("ringtwit", "onresume");
+
+		}
+
+	}
+
+	public void onClick(View v) {
+
+		switch (v.getId()) {
+
+		// 通話監視サービス開始ボタンの場合
+		case R.id.button1:
+			Intent startServiceIntent = new Intent(RingTwitActivity.this,
+					RingTwitService.class);
+			startService(startServiceIntent);
+			break;
+
+		// twitter認証ボタンの場合
+		case R.id.button2:
+			executeOauth();
+			break;
+
+		}
+
+	}
+
+	// 認証ページをブラウザで開く
+	private void executeOauth() {
+		Log.d("ringtwit", "executeOauth");
+
+		// Twitetr4jの設定を読み込む
+		Configuration conf = ConfigurationContext.getInstance();
+
+		// Oauth認証オブジェクト作成
+		OAuthAuthorization oauth = new OAuthAuthorization(conf);
+		// Oauth認証オブジェクトにconsumerKeyとconsumerSecretを設定
+		oauth.setOAuthConsumer("ztK1xRFV31ma6O0a3jiQ",
+				"HFiekYjyOzMdjm1B7XjuWo2tiFf9ZBo7CZujPrrw4");
+
+		// アプリの認証オブジェクト作成
+		RequestToken _req = null;
+		try {
+			_req = oauth.getOAuthRequestToken();
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
+		String _uri;
+		_uri = _req.getAuthorizationURL();
+		// _uriのログ
+		Log.d("ringtwit", _uri);
+		startActivityForResult(new Intent(Intent.ACTION_VIEW, Uri.parse(_uri)),
+				0);
+
+	}
+
 }
---
+++ RingTwit/src/jp/gr/java_conf/ringtwit/RingTwitService.java
@@ -0,0 +1,50 @@
+package jp.gr.java_conf.ringtwit;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+public class RingTwitService extends Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+
+		return null;
+	}
+
+	public void onCreate() {
+
+	}
+
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		//確認用トースト
+		ToastNumber("start");
+
+		//電話情報の受信開始
+		TelephonyManager telManager=(TelephonyManager)
+		            getSystemService(Context.TELEPHONY_SERVICE);
+		        telManager.listen(phoneStateListener,
+		            PhoneStateListener.LISTEN_CALL_STATE|
+		            PhoneStateListener.LISTEN_SERVICE_STATE|
+		            PhoneStateListener.LISTEN_SIGNAL_STRENGTH|
+		            PhoneStateListener.LISTEN_CELL_LOCATION);
+		return START_STICKY;
+	}
+
+	public PhoneStateListener phoneStateListener = new PhoneStateListener(){
+	    @Override
+	    public void onCallStateChanged(int state, String number) {
+	    	  if (state==TelephonyManager.CALL_STATE_RINGING) ToastNumber(number);
+	    }
+	};
+
+	//トースト
+	public void ToastNumber(String number2){
+		Toast.makeText(this,number2, Toast.LENGTH_LONG).show();
+	}
+
+}
